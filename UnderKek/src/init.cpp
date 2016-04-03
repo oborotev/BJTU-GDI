f3a367c395cc9f3ddf92f5e4920ee732fcf3bdeb
oborotev <stormsnaps@gmail.com>
2016-04-03 09:34:14 +0000

    init of heart

---
+++ UnderKek/src/init.cpp
@@ -0,0 +1,94 @@
+//
+// Created by storm on 03/04/16.
+//
+
+# include "underkek.h"
+
+const int   Underkek::playerInit()
+{
+    this->_mediaHandler->addNewTexture("../media/textures/frisk.png", "frisky");
+    this->_graphicHandler->initPlayer(200, 200, 20, 2.5, true, sf::seconds(0.2), this->_mediaHandler->getTexture("frisky"));
+    //Animation Still
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::STILL, sf::IntRect(0, 0, 38, 57));
+    //Animation UP
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::UP, sf::IntRect(48, 68, 38, 57));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::UP, sf::IntRect(0, 68, 38, 57));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::UP, sf::IntRect(96, 68, 38, 57));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::UP, sf::IntRect(0, 68, 38, 57));
+    //Animation DOWN
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::DOWN, sf::IntRect(48, 0, 38, 57));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::DOWN, sf::IntRect(0, 0, 38, 57));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::DOWN, sf::IntRect(96, 0, 38, 57));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::DOWN, sf::IntRect(0, 0, 38, 57));
+    //Animation RIGHT
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::RIGHT, sf::IntRect(50, 136, 38, 59));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::RIGHT, sf::IntRect(0, 136, 38, 59));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::RIGHT, sf::IntRect(50, 136, 38, 59));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::RIGHT, sf::IntRect(0, 136, 38, 59));
+    //Animation LEFT
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::LEFT, sf::IntRect(0, 204, 38, 59));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::LEFT, sf::IntRect(50, 204, 38, 59));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::LEFT, sf::IntRect(0, 204, 38, 59));
+    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::LEFT, sf::IntRect(50, 204, 38, 59));
+}
+
+const int   Underkek::init()
+{
+    this->_combatMode = false;
+    _tiledef.fill(TilesetHandler::WALL);
+    _tiledef[9] = TilesetHandler::FREE;
+    this->_graphicHandler = new GraphicHandler("Underkek", "../media/fonts/DTM-Mono.otf");
+    this->_tilesetHandler = this->_graphicHandler->getBaseMap();
+    this->_mediaHandler = this->_graphicHandler->getMediaHandler();
+    this->_physicsHandler = this->_graphicHandler->getPhysicsHandler();
+    this->_mediaHandler->addNewTexture("../media/textures/tileset.gif", "map_tileset");
+    this->_mediaHandler->addNewTexture("../media/textures/heart.jpe", "soul_heart");
+    this->_mediaHandler->addNewSprite(this->_mediaHandler->getTexture("soul_heart"), "player_soul");
+    this->playerInit();
+    this->_tilesetHandler->init(this->_mediaHandler->getTexture("map_tileset"), sf::Vector2u(32, 32), "../media/maps/base", 0, 0, _tiledef.data());
+    if (this->_graphicHandler->init()) {
+        std::cout << "There was a problem with the graphical initialization" << std::endl;
+        return 1;
+    }
+    this->_physicsHandler = this->_graphicHandler->getPhysicsHandler();
+    this->_graphicHandler->getCamera()->updatePositionCenter(1024/2, 768/2);
+    this->initCombatMode();
+    return (0);
+}
+
+const int Underkek::initCombatMode()
+{
+    /* Combat Box */
+    b2BodyDef myBodyDef;
+    myBodyDef.type = b2_staticBody;
+    myBodyDef.position.Set(0, 0);
+    this->_physicsHandler->registerNewBody(this->_physicsHandler->getWorld()->CreateBody(&myBodyDef), "combat_box");
+    //shape definition
+    b2PolygonShape polygonShape;
+    //fixture definition
+    b2FixtureDef myFixtureDef;
+    myFixtureDef.shape = &polygonShape;
+    //add four walls to the static body
+    polygonShape.SetAsBox(92, 2.5, b2Vec2(1024/2, 400), 0);//ground
+    this->_physicsHandler->getBody("combat_box")->CreateFixture(&myFixtureDef);
+    polygonShape.SetAsBox(92, 2.5, b2Vec2(1024/2, 600), 0);//ceiling
+    this->_physicsHandler->getBody("combat_box")->CreateFixture(&myFixtureDef);
+    polygonShape.SetAsBox(2.5, 102.5, b2Vec2(417, 500), 0);//left wall
+    this->_physicsHandler->getBody("combat_box")->CreateFixture(&myFixtureDef);
+    polygonShape.SetAsBox(2.5, 102.5, b2Vec2(608, 500), 0);//right wall
+    this->_physicsHandler->getBody("combat_box")->CreateFixture(&myFixtureDef);
+
+    /* Soul */
+    b2BodyDef soulDef;
+    soulDef.type = b2_staticBody;
+    soulDef.position = b2Vec2(500, 450);
+    this->_physicsHandler->registerNewBody(this->_physicsHandler->getWorld()->CreateBody(&soulDef), "player_soul");
+
+    b2PolygonShape boxShape;
+    boxShape.SetAsBox(32, 32);
+    b2FixtureDef soulFixtureDef;
+    soulFixtureDef.density = 1.0f;
+    soulFixtureDef.friction = 0.3f;
+    soulFixtureDef.shape = &boxShape;
+    this->_physicsHandler->getBody("player_soul")->CreateFixture(&soulFixtureDef);
+}
--- UnderKek/src/underkek.cpp
+++ UnderKek/src/underkek.cpp
@@ -8,80 +8,6 @@ Underkek::~Underkek() {
     delete this->_graphicHandler;
 }
 
-const int   Underkek::playerInit()
-{
-    this->_mediaHandler->addNewTexture("../media/textures/frisk.png", "frisky");
-    this->_graphicHandler->initPlayer(200, 200, 20, 2.5, true, sf::seconds(0.2), this->_mediaHandler->getTexture("frisky"));
-    //Animation Still
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::STILL, sf::IntRect(0, 0, 38, 57));
-    //Animation UP
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::UP, sf::IntRect(48, 68, 38, 57));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::UP, sf::IntRect(0, 68, 38, 57));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::UP, sf::IntRect(96, 68, 38, 57));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::UP, sf::IntRect(0, 68, 38, 57));
-    //Animation DOWN
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::DOWN, sf::IntRect(48, 0, 38, 57));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::DOWN, sf::IntRect(0, 0, 38, 57));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::DOWN, sf::IntRect(96, 0, 38, 57));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::DOWN, sf::IntRect(0, 0, 38, 57));
-    //Animation RIGHT
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::RIGHT, sf::IntRect(50, 136, 38, 59));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::RIGHT, sf::IntRect(0, 136, 38, 59));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::RIGHT, sf::IntRect(50, 136, 38, 59));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::RIGHT, sf::IntRect(0, 136, 38, 59));
-    //Animation LEFT
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::LEFT, sf::IntRect(0, 204, 38, 59));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::LEFT, sf::IntRect(50, 204, 38, 59));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::LEFT, sf::IntRect(0, 204, 38, 59));
-    this->_graphicHandler->getPlayer()->addFrameState(LivingEntity::Direction::LEFT, sf::IntRect(50, 204, 38, 59));
-}
-
-const int   Underkek::init()
-{
-    this->_combatMode = false;
-    _tiledef.fill(TilesetHandler::WALL);
-    _tiledef[9] = TilesetHandler::FREE;
-    this->_graphicHandler = new GraphicHandler("Underkek", "../media/fonts/DTM-Mono.otf");
-    this->_tilesetHandler = this->_graphicHandler->getBaseMap();
-    this->_mediaHandler = this->_graphicHandler->getMediaHandler();
-    this->_physicsHandler = this->_graphicHandler->getPhysicsHandler();
-    this->_mediaHandler->addNewTexture("../media/textures/tileset.gif", "map_tileset");
-    this->playerInit();
-    this->_tilesetHandler->init(this->_mediaHandler->getTexture("map_tileset"), sf::Vector2u(32, 32), "../media/maps/base", 0, 0, _tiledef.data());
-    if (this->_graphicHandler->init()) {
-        std::cout << "There was a problem with the graphical initialization" << std::endl;
-        return 1;
-    }
-    this->_physicsHandler = this->_graphicHandler->getPhysicsHandler();
-    this->_graphicHandler->getCamera()->updatePositionCenter(1024/2, 768/2);
-    this->initCombatMode();
-    return (0);
-}
-
-
-
-const int Underkek::initCombatMode()
-{
-    b2BodyDef myBodyDef;
-    myBodyDef.type = b2_staticBody;
-    myBodyDef.position.Set(0, 0);
-    this->_physicsHandler->registerNewBody(this->_physicsHandler->getWorld()->CreateBody(&myBodyDef), "combat_box");
-    //shape definition
-    b2PolygonShape polygonShape;
-    //fixture definition
-    b2FixtureDef myFixtureDef;
-    myFixtureDef.shape = &polygonShape;
-    //add four walls to the static body
-    polygonShape.SetAsBox(92/RATIO, 2.5/RATIO, b2Vec2(1024/2/RATIO, 400/RATIO), 0);//ground
-    this->_physicsHandler->getBody("combat_box")->CreateFixture(&myFixtureDef);
-    polygonShape.SetAsBox(92/RATIO, 2.5/RATIO, b2Vec2(1024/2/RATIO, 600/RATIO), 0);//ceiling
-    this->_physicsHandler->getBody("combat_box")->CreateFixture(&myFixtureDef);
-    polygonShape.SetAsBox(2.5/RATIO, 102.5/RATIO, b2Vec2(417/RATIO, 500/RATIO), 0);//left wall
-    this->_physicsHandler->getBody("combat_box")->CreateFixture(&myFixtureDef);
-    polygonShape.SetAsBox(2.5/RATIO, 102.5/RATIO, b2Vec2(608/RATIO, 500/RATIO), 0);//right wall
-    this->_physicsHandler->getBody("combat_box")->CreateFixture(&myFixtureDef);
-}
-
 const int        Underkek::wanderlust()
 {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
@@ -106,8 +32,12 @@ const int   Underkek::start() {
             if (this->_graphicHandler->eventTriggered(sf::Event::Closed))
                 this->_graphicHandler->terminate();
         if (this->_graphicHandler->getIsAlive()) {
+            this->_physicsHandler->getWorld()->Step(1/60.f, 8, 3);
             if (!this->_combatMode)
                 this->_graphicHandler->drawPolygonFromFixtures(this->_physicsHandler->getBody("combat_box")->GetFixtureList());//this->wanderlust();
+            this->_mediaHandler->getSprite("player_soul")->setPosition(this->_physicsHandler->getBody("player_soul")->GetPosition().x, this->_physicsHandler->getBody("player_soul")->GetPosition().y);
+            this->_mediaHandler->getSprite("player_soul")->setRotation(180/b2_pi * this->_physicsHandler->getBody("player_soul")->GetAngle());
+            this->_graphicHandler->draw(*this->_mediaHandler->getSprite("player_soul"));
             this->_graphicHandler->loop();
         }
     }
