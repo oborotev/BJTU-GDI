3426283ae9fbbbe669c726919e78ed4a76b78e1f
oborotev <stormsnaps@gmail.com>
2016-04-19 18:51:28 +0000

    finished basis of boxAnimations architecture with map of method pointers

--- GameEngine/KekEngine/includes/boxAnimations.h
+++ GameEngine/KekEngine/includes/boxAnimations.h
@@ -17,26 +17,30 @@ public:
     enum ANIMATIONS {
         SIZE_CHANGE = 0
     };
+    typedef struct s_animationComponents
+    {
+        b2Fixture         *fixtures;
+        BoxAnimations::ANIMATIONS animationType;
+        sf::IntRect newSize;
+        float       newAngle;
+        float       speed;
+        sf::Time    duration;
+    }              t_animationComponents;
+    typedef bool (BoxAnimations::*Animator)(std::vector<t_animationComponents>::iterator animation);
 public:
     BoxAnimations(const sfx::FrameClock *clock);
     void init();
     void registerNewAnimation(b2Fixture *fixtures, const BoxAnimations::ANIMATIONS &animationType, const sf::IntRect &newSize, const float &newAngle, const float &speed=1.0, const sf::Time &duration=sf::seconds(0));
     void terminate();
     void animationsHandler();
+    //Animators
+    bool sizeChange(std::vector<t_animationComponents>::iterator animation);
 private:
-    typedef struct s_animationComponents
-    {
-        b2Fixture         *fixtures;
-        const BoxAnimations::ANIMATIONS &animationType;
-        const sf::IntRect newSize;
-        const float       newAngle;
-        const float       speed;
-        const sf::Time    duration;
-    }              t_animationComponents;
     const sfx::FrameClock *_clock;
     sf::Thread  *_thread;
     sf::Mutex   _mutex;
     std::vector<t_animationComponents> _animations;
+    std::map<const BoxAnimations::ANIMATIONS, Animator> _animators;
     bool _isAlive;
 };
 
--- GameEngine/KekEngine/src/boxAnimations.cpp
+++ GameEngine/KekEngine/src/boxAnimations.cpp
@@ -12,24 +12,51 @@ BoxAnimations::BoxAnimations(const sfx::FrameClock *clock)
 
 void BoxAnimations::init()
 {
+    this->_animators.insert(std::make_pair(BoxAnimations::ANIMATIONS::SIZE_CHANGE, &BoxAnimations::sizeChange));
     this->_isAlive = true;
     this->_thread = new sf::Thread(&BoxAnimations::animationsHandler, this);
+    this->_thread->launch();
 }
 
 void BoxAnimations::registerNewAnimation(b2Fixture *fixtures, const BoxAnimations::ANIMATIONS &animationType,
                                          const sf::IntRect &newSize, const float &newAngle, const float &speed, const sf::Time &duration)
 {
+    t_animationComponents newComponent;
+
+    newComponent.fixtures = fixtures;
+    newComponent.animationType = animationType;
+    newComponent.newSize = newSize;
+    newComponent.newAngle = newAngle;
+    newComponent.speed = speed;
+    newComponent.duration = duration;
+    this->_mutex.lock();
+    this->_animations.emplace_back(newComponent);
+    this->_mutex.unlock();
     //this->_threadPool.push_back(new sf::Thread(std::bind(&sizeChanger, this, this->_clock, fixtures, animationType, newSize, this->_threadPool.size(), speed)));
     //this->_threadPool.back()->launch();
     return ;
+}
+
+bool BoxAnimations::sizeChange(std::vector<t_animationComponents>::iterator animation)
+{
+    std::cout << animation->animationType << std::endl;
+    return (false);
 }
 
 void BoxAnimations::animationsHandler()
 {
     while (this->_isAlive)
     {
-        if (this->_animations.size())
+        for (std::vector<t_animationComponents>::iterator it = this->_animations.begin() ; it != this->_animations.end(); ++it) {
             std::cout << "There is an animation" << std::endl;
+            if (this->_animations.size() < 1)
+                break ;
+            if (!(this->*_animators[it->animationType])(it)) {
+                this->_mutex.lock();
+                this->_animations.erase(it);
+                this->_mutex.unlock();
+            }
+        }
     }
 }
 
